cc58e124518f0fa9425e4c7e0798d752b5917e1e
vyacheslav.sviridov <vyacheslav.sviridov@t2.ru>
2026-03-11 17:38:30 +0600

    homework-3

---
+++ DatabaseAndModels/shopapp/management/commands/create_orders.py
@@ -0,0 +1,16 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
+from shopapp.models import Order, Product
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        user, _ = User.objects.get_or_create(username="admin")
+
+        order, created = Order.objects.get_or_create(
+            user=user,
+            delivery_address="Lenina St. 123",
+            defaults={"promocode": "SALE"}
+        )
+        
+        self.stdout.write(self.style.SUCCESS(f"Order for {user.username} "
+                          f"{'created' if created else 'updated'} {order}"))
