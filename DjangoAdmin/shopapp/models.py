1f298fcb8d05bf3d143cbfb331253349d77ecdda
Vyacheslav Sviridov <atx@inbox.ru>
2026-03-12 19:18:36 +0600

    homework-4 done

---
+++ DjangoAdmin/shopapp/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import User
+
+
+class Product(models.Model):
+    class Meta:
+        ordering = ['name']
+
+    name = models.CharField(max_length=255)
+    description = models.TextField(null=False, blank=True)
+    price = models.DecimalField(default=0, max_digits=10, decimal_places=2)
+    discount = models.PositiveSmallIntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    archived = models.BooleanField(default=True)
+
+    @property
+    def description_short(self) -> str:
+        if len(self.description) < 48:
+            return self.description
+        return self.description[:48] + '...'
+
+    def __str__(self):
+        return f'Product({self.pk}, {self.name})'
+
+
+class Order(models.Model):
+    delivery_address = models.TextField(null=False, blank=True)
+    promocode = models.CharField(max_length=20, null=False, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    products = models.ManyToManyField(Product, related_name='orders')
+
+    def __str__(self):
+        return f'Order #{self.pk} by {self.user}'
