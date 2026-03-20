d8c206532a92fd9ecf7aaf8180911f524e644a56
vyacheslav.sviridov <vyacheslav.sviridov@t2.ru>
2026-03-20 15:42:24 +0600

    FEA: forms additional info

--- Forms/shopapp/forms.py
+++ Forms/shopapp/forms.py
@@ -33,4 +33,43 @@ class OrderForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        # self.fields['products'].queryset = Product.objects.only('id', 'name')
         self.fields['products'].queryset = Product.objects.filter(archived=False)
+
+
+# class AuditMixin:
+#     """
+#     Миксин для автоматического заполнения полей created_by и updated_by.
+#     Используется в ModelForm.
+#     Требует, чтобы форма получила request через инициализацию.
+#     """
+
+#     def __init__(self, *args, **kwargs):
+#         self.request = kwargs.pop('request', None)  # ← извлекаем request из kwargs
+#         super().__init__(*args, **kwargs)
+
+#     def save(self, commit=True):
+#         instance = super().save(commit=False)
+#         if self.request and self.request.user.is_authenticated:
+#             if not instance.pk:  # если объект новый
+#                 instance.created_by = self.request.user
+#             instance.updated_by = self.request.user
+#         if commit:
+#             instance.save()
+#             self.save_m2m()  # важно для ManyToManyField
+#         return instance
+
+# class OrderForm(AuditMixin, forms.ModelForm):
+    # ... поля формы
+
+
+# class SecureForm(forms.Form):
+#     # Защита от массового присвоения
+#     def __init__(self, *args, **kwargs):
+#         self.request = kwargs.pop('request', None)
+#         super().__init__(*args, **kwargs)
+   
+#     # CSRF защита для AJAX-форм
+#     @method_decorator(requires_csrf_token)
+#     def dispatch(self, request, *args, **kwargs):
+#         return super().dispatch(request, *args, **kwargs)
