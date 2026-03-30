98d75483da376084338508ed9c38f7324842138a
vyacheslav.sviridov <vyacheslav.sviridov@t2.ru>
2026-03-30 17:59:14 +0600

    homework-10

---
+++ permissions/myauth/views.py
@@ -0,0 +1,64 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required, permission_required, user_passes_test
+from django.http import HttpRequest, HttpResponse
+from django.contrib.auth.views import LogoutView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView, CreateView
+
+from .models import Profile
+
+
+@user_passes_test(lambda u: u.is_superuser)
+def set_cookie_view(request: HttpRequest) -> HttpResponse:
+    response = HttpResponse('Cookie set')
+    response.set_cookie('fizz', 'buzz', max_age=3600)
+    return response
+
+
+def get_cookie_view(request: HttpRequest) -> HttpResponse:
+    value = request.COOKIES.get('fizz', 'default value')
+    return HttpResponse(f'Cookie value {value!r}')
+
+
+@permission_required('view_profile', raise_exception=True)
+def set_session_view(request: HttpRequest) -> HttpResponse:
+    request.session['foobar'] = 'spameggs'
+    return HttpResponse('Session set!')
+
+
+@login_required
+def get_session_view(request: HttpRequest) -> HttpResponse:
+    value = request.session.get('foobar', 'default')
+    return HttpResponse(f'Session value {value!r}')
+
+
+class AboutMeView(TemplateView):
+    template_name = 'myauth/about-me.html'
+
+
+class RegisterView(CreateView):
+    form_class = UserCreationForm
+    template_name = "myauth/register.html"
+    success_url = reverse_lazy('myauth:about-me')
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        Profile.objects.create(user=self.object)
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password1')
+        user = authenticate(
+            self.request,
+            username=username,
+            password=password,
+        )
+        login(request=self.request, user=user)
+        return response
+
+class MyLogoutView(TemplateView):
+    template_name = 'myauth/logout.html'
+    # http_method_names = ["get", "post", "options"]
+    # def get(self, request, *args, **kwargs):
+    #     logout(request)
+    #     return redirect(self.next_page)
