21db1ac8f70dd8607d30d775e9ac4a31295e15e4
vyacheslav.sviridov <vyacheslav.sviridov@t2.ru>
2026-04-23 14:48:55 +0600

    hw14 started

---
+++ REST/myauth/urls.py
@@ -0,0 +1,31 @@
+from django.urls import path, include
+from django.contrib.auth.views import LoginView, LogoutView
+from .views import (get_cookie_view, set_cookie_view, get_session_view,
+                    set_session_view, MyLogoutView, AboutMeView, RegisterView,
+                    UserDetailView, UserUpdateView, UserListView, HelloView)
+
+
+app_name = 'myauth'
+
+urlpatterns = [
+    path(
+        'login/',
+        LoginView.as_view(
+            template_name='myauth/login.html',
+            redirect_authenticated_user=True,
+            ),
+        name='login'
+    ),
+    path('hello/', HelloView.as_view(), name='hello'),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('logout-confirm/', MyLogoutView.as_view(), name='logout-confirm'),
+    path('users/', UserListView.as_view(), name='users-list'),
+    path('about-me/', AboutMeView.as_view(), name='about-me'),
+    path('users/<pk>/', UserDetailView.as_view(), name='user-detail'),
+    path('users/<pk>/edit/', UserUpdateView.as_view(), name='user-edit'),
+    path('cookie/get/', get_cookie_view, name='cookie-get'),
+    path('cookie/set/', set_cookie_view, name='cookie-set'),
+    path('session/get/', get_session_view, name='session-get'),
+    path('session/set/', set_session_view, name='session-set'),
+]
