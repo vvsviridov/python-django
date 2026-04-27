07bbc6c6775fe9725bf52ccc9892652999c664fd
Vyacheslav Sviridov <atx@inbox.ru>
2026-04-27 19:00:03 +0600

    homework-16 done

--- queries/blogapp/urls.py
+++ queries/blogapp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from .views import ArticleListView
 
-app_name = "blogapp"
+
+app_name = 'blogapp'
 
 urlpatterns = [
-    path("articles/", ArticleListView.as_view(), name="article_list"),
+    path('articles/', ArticleListView.as_view(), name='article_list'),
 ]
--- queries/blogapp/views.py
+++ queries/blogapp/views.py
@@ -1,12 +1,14 @@
 from django.views.generic import ListView
 from .models import Article
 
+
 class ArticleListView(ListView):
     queryset = (
         Article.objects
-        .select_related("author", "category")
-        .prefetch_related("tags")
-        .all()
+            .select_related('author', 'category')
+            .prefetch_related('tags')
+            .defer('content', 'author__bio')
+            .all()
     )
-    template_name = "blogapp/article_list.html"
-    context_object_name = "articles"
+    template_name = 'blogapp/article_list.html'
+    context_object_name = 'articles'
